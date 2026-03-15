74bcaa98f3474c288791101faaa8c49c8690452a
Ahmed <a20mus1im633@gmail.com>
2026-03-15 13:31:45 +0200

    Initial commit

---
+++ src/Manager.java
@@ -0,0 +1,16 @@
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+
+public class Manager {
+
+    void Load(){
+
+    }
+    void save(boolean SameFile){
+
+    }
+}
