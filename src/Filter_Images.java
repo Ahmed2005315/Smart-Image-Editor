e1bcf859b0918c5729866fce73b783f459d203f8
Ahmed <a20mus1im633@gmail.com>
2026-03-25 21:58:41 +0200

    Add resize, ColorFilter

--- src/Filter_Images.java
+++ src/Filter_Images.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.awt.image.BufferedImage;
 public class Filter_Images {
     //crop
@@ -21,6 +22,7 @@ public class Filter_Images {
 
         return rotated;
     }
+
     BufferedImage GrayScale(BufferedImage img){
 
         int height= img.getHeight();
@@ -40,6 +42,7 @@ public class Filter_Images {
         }
         return grayImg;
     }
+
     BufferedImage BlackWhite(BufferedImage img){
         int threshold=128;
         int height= img.getHeight();
@@ -60,4 +63,94 @@ public class Filter_Images {
         }
         return BWImg;
     }
+
+BufferedImage resize(BufferedImage img,int New_Width,int New_Height)
+{
+    BufferedImage resized=new BufferedImage(New_Width,New_Height,img.getType());
+    double Percent_Width= (double)(img.getWidth()/New_Width);
+    double Percent_Height= (double)(img.getHeight()/New_Height);
+    for(int x=0;x<New_Width;++x){
+        for(int y=0;y<New_Height;++y){
+            int xOrigin = Math.min((int)(x * Percent_Width), img.getWidth() - 1);
+            int yOrigin = Math.min((int)(y * Percent_Height), img.getHeight() - 1);
+            resized.setRGB(x, y, img.getRGB(xOrigin, yOrigin));
+        }
+    }
+    return resized;
 }
+
+enum ColorsFilter
+{
+        RED,
+        GREEN,
+        CYAN,
+        BLUE,
+        YELLOW,
+        MAGENTA
+}
+int clamp(int v){
+        return Math.max(0,Math.min(255,v));
+}
+BufferedImage applyColorFilter(BufferedImage img,ColorsFilter color)
+{
+BufferedImage result=new BufferedImage(img.getWidth(),img.getHeight(),img.getType());
+    double boost = 1.5;
+    double reduce = 1.0 / boost;
+
+    for (int y = 0; y < img.getHeight(); ++y) {
+        for (int x = 0; x < img.getWidth(); ++x) {
+
+            int RGB = img.getRGB(x, y);
+
+            int A = (RGB >> 24) & 0xff;
+            int R = (RGB >> 16) & 0xff;
+            int G = (RGB >> 8) & 0xff;
+            int B = RGB & 0xff;
+
+            int newR = R, newG = G, newB = B;
+
+            switch (color) {
+                case RED:
+                    newR=clamp((int)(R*boost));
+                    newG=clamp((int)(G*reduce));
+                    newB=clamp((int)(B*reduce));
+                    break;
+
+                case GREEN:
+                    newR = clamp((int)(R * reduce));
+                    newG = clamp((int)(G * boost));
+                    newB = clamp((int)(B * reduce));
+                    break;
+
+                case BLUE:
+                    newR = clamp((int)(R * reduce));
+                    newG = clamp((int)(G * reduce));
+                    newB = clamp((int)(B * boost));
+                    break;
+
+                case CYAN:
+                    newR = clamp((int)(R * reduce));
+                    newG = clamp((int)(G * boost));
+                    newB = clamp((int)(B * boost));
+                    break;
+
+                case YELLOW:
+                    newR = clamp((int)(R * boost));
+                    newG = clamp((int)(G * boost));
+                    newB = clamp((int)(B * reduce));
+                    break;
+
+                case MAGENTA:
+                    newR = clamp((int)(R * boost));
+                    newG = clamp((int)(G * reduce));
+                    newB = clamp((int)(B * boost));
+                    break;
+            }
+        int NewRGB=(A<<24)|(newR<<16)|(newG<<8)|newB;
+        result.setRGB(x,y,NewRGB);
+    }
+}
+return result;
+}
+}
+
